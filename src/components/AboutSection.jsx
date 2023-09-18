fc187bcc7cc722a3630212a4777f4688c2ff7b92
Kamsnyc <kamel.singh1999@gmail.com>
2023-09-18 16:33:27 -0400

    created skills section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function AboutSection() {
   return (
-    <section id="about" className="w-full md:h-screen p-2 flex py-12 items-center">
+    <section id="about" className="w-full p-2 flex items-center">
   <div className="max-w-[1248px] m-auto md:grid grid-cols-3 gap-8">
     <div className="col-span-2">
       <p className="uppercase text-xl tracking-widest text-[#FD8B49]">About</p>
       <h2 className="py-4">Who I Am</h2>
-      <p>// I am not your normal developer</p>
+      <p>// Your future developer</p>
       <p className="xl:pr-20 py-4">
         I'm Kamel, also recognized as @KamsNyc. I specialize in crafting UI
         designs on Figma and developing full-stack websites. Currently enhancing
@@ -21,13 +21,15 @@ function AboutSection() {
         weekends and thrive on creative pursuits beyond my roles in e-commerce
         and eBay.
       </p>
-      <p className="py-2 underline cursor-pointer">
-        Check out some of my latest projects
+
+  
+      <p className="py-2">
+        <p>Check out the <a className=' font-bold underline hover:text-[#FD8E4E] ease-in duration-300' href=''>Full about me</a> or <a className=' font-bold underline hover:text-[#FD8E4E] ease-in duration-300' href=''>some of my latest projects</a></p>
       </p>
     </div>
     <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
       <img
-        className="rounded-xl mt-8"
+        className="rounded-xl"
         src="https://source.unsplash.com/1500x2000/?abstract"
         alt="test"
       />
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,19 +2,22 @@ import React from "react";
 
 function Skills() {
   return (
-    <section className="w-full lg:h-screen p-2">
-      <div className="max-w-[1248px] mx-auto flex flex-col justify-center h-full">
+    <section className="w-full pt-24 p-2">
+
+      {/* FRONT END SKILLS */}
+
+      <div className="max-w-[1248px] mx-auto flex flex-col justify-center h-full ">
+      <h2 className="py-4">What I Can Do</h2>
         <p className="text-xl tracking-widest uppercase text-[#FD8B49]">
-          Skills
+          Fontend Skills
         </p>
-        <h2 className="py-4">What I Can Do</h2>
-        <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className=" pt-4 grid md:grid-cols-3 lg:grid-cols-6 gap-4">
           {/* HTML */}
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <img
-                  src="/../src/assets/html.png"
+                  src="/../src/assets/Html.png"
                   alt="HTML5 Icon"
                   className="w-12 h-12"
                 />
@@ -30,7 +33,7 @@ function Skills() {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <img
-                  src="/../src/assets/css.png"
+                  src="/../src/assets/Css.png"
                   alt="CSS Icon"
                   className="w-12 h-12"
                 />
@@ -46,7 +49,7 @@ function Skills() {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <img
-                  src="/../src/assets/javascript.png"
+                  src="/../src/assets/Javascript.png"
                   alt="JavaScript Icon"
                   className="w-12 h-12"
                 />
@@ -57,77 +60,13 @@ function Skills() {
             </div>
           </div>
 
-          {/* Node.js */}
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <img
-                  src="/../src/assets/NodeJs.png"
-                  alt="Node.js Icon"
-                  className="w-12 h-12"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Node.js</h3>
-              </div>
-            </div>
-          </div>
-
-          {/* Figma */}
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <img
-                  src="/../src/assets/Figma.png"
-                  alt="Figma Icon"
-                  className="w-12 h-12"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Figma</h3>
-              </div>
-            </div>
-          </div>
-
-          {/* Python */}
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <img
-                  src="/../src/assets/Python.png"
-                  alt="Python Icon"
-                  className="w-12 h-12"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Python</h3>
-              </div>
-            </div>
-          </div>
-
-          {/* Lightroom */}
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <img
-                  src="/../src/assets/Lightroom.png"
-                  alt="Lightroom Icon"
-                  className="w-12 h-12"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Lightroom</h3>
-              </div>
-            </div>
-          </div>
-
           {/* React */}
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <img
                   src="/../src/assets/React.png"
-                  alt="React Icon"
+                  alt="JavaScript Icon"
                   className="w-12 h-12"
                 />
               </div>
@@ -137,24 +76,148 @@ function Skills() {
             </div>
           </div>
 
-          {/* VS Code */}
+          {/* Tailwind */}
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <img
-                  src="/../src/assets/VsCode.png"
-                  alt="VS Code Icon"
+                  src="/../src/assets/Tailwind.png"
+                  alt="JavaScript Icon"
                   className="w-12 h-12"
                 />
               </div>
               <div className="flex flex-col items-center justify-center">
-                <h3>VS Code</h3>
+                <h3>Tailwind</h3>
               </div>
             </div>
           </div>
+
+         {/* ADD NEW */}
+
         </div>
+
+        {/* Backend */}
+        <section className="w-full pt-12 p-2">
+        <div className="max-w-[1248px] mx-auto flex flex-col justify-center h-full">
+        <p className="text-xl tracking-widest uppercase text-[#FD8B49]">
+          Backend Skills
+        </p>
+        <div className=" pt-4 grid md:grid-cols-3 lg:grid-cols-6 gap-4">
+
+          {/* Nodejs */}
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <img
+                  src="/../src/assets/Nodejs.png"
+                  alt="HTML5 Icon"
+                  className="w-12 h-12"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Node.js</h3>
+              </div>
+            </div>
+          </div>
+
+          {/* Firebase */}
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <img
+                  src="/../src/assets/Firebase.png"
+                  alt="HTML5 Icon"
+                  className="w-12 h-12"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Firebase</h3>
+              </div>
+            </div>
+          </div>
+          </div>
+          </div>
+          </section>
+
+           {/* ETC Skills */}
+        <section className="w-full pt-12 p-2">
+        <div className="max-w-[1248px] mx-auto flex flex-col justify-center h-full">
+        <p className="text-xl tracking-widest uppercase text-[#FD8B49]">
+          More Skills
+        </p>
+        <div className=" pt-4 grid md:grid-cols-3 lg:grid-cols-6 gap-4">
+
+          {/* Figma */}
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <img
+                  src="/../src/assets/Figma.png"
+                  alt="HTML5 Icon"
+                  className="w-12 h-12"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Figma</h3>
+              </div>
+            </div>
+          </div>
+
+          {/* Wordpress */}
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <img
+                  src="/../src/assets/Wordpress.png"
+                  alt="HTML5 Icon"
+                  className="w-12 h-12"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Wordpress</h3>
+              </div>
+            </div>
+          </div>
+
+          {/* Wix */}
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <img
+                  src="/../src/assets/Wix.png"
+                  alt="HTML5 Icon"
+                  className="w-12 h-12"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Wix</h3>
+              </div>
+            </div>
+          </div>
+
+          {/* Shopify */}
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <img
+                  src="/../src/assets/Shopify.png"
+                  alt="HTML5 Icon"
+                  className="w-12 h-12"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Shopify</h3>
+              </div>
+            </div>
+          </div>
+          </div>
+          </div>
+          </section>
+
       </div>
     </section>
+
   );
 }
 
